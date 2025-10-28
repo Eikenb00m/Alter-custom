425c27252ae315921a0c29dbf601489ebb018702
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-10-28 20:26:06 +0100

    Fix content build dependency cycle

--- content/build.gradle.kts
+++ content/build.gradle.kts
@@ -5,7 +5,6 @@ description = "Server Content"
 dependencies {
     implementation(projects.util)
     implementation(project(":game-api"))
-    implementation(project(":game-server"))
     implementation(kotlin("script-runtime"))
     implementation(kotlin("scripting-jvm"))
     implementation(kotlin("scripting-jvm-host"))
@@ -13,10 +12,6 @@ dependencies {
     implementation("dev.or2:server-utils:0.7")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
     implementation(rootProject.project.libs.rsprot)
-}
-
-tasks.withType<KotlinCompile> {
-    dependsOn(":game-server:build")
 }
 
 tasks.named<Jar>("jar") {
